bcc98b7132ac464872cf898cc303bd5239bc825d
Imasha Samarasinghe <132127978+Imashims@users.noreply.github.com>
2024-06-13 19:02:01 +0530

    adding new place just work fine

--- api/index.js
+++ api/index.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
-const { default: mongoose } = require('mongoose');
+const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('./models/User.js');
+const Place = require('./models/Place.js');
 const cookieParser = require('cookie-parser');
-require('dotenv').config();
+const imageDownloader = require('image-downloader');
+const multer = require('multer');
+const fs = require('fs');
 
+require('dotenv').config();
 const app = express();
 
 const bcryptSalt = bcrypt.genSaltSync(10);
@@ -14,12 +19,15 @@ const jwtSecret = 'qugfuyf434v';
 
 app.use(express.json());
 app.use(cookieParser());
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(cors({
     credentials: true,
     origin: ['http://localhost:5173', 'http://localhost:5174']
 }));
 
-mongoose.connect(process.env.MONGO_URL);
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Error connecting to MongoDB:', err));
 
 app.get('/test', (req, res) => {
     res.json('test ok');
@@ -77,9 +85,70 @@ app.get('/profile', (req, res) => {
     }
 });
 
-app.post('/logout', (req,res) => {
+app.post('/logout', (req, res) => {
     res.cookie('token', '').json(true);
-})
+});
+
+app.post('/upload-by-link', async (req, res) => {
+    const { link } = req.body;
+    const newName = 'photo' + Date.now() + '.jpg';
+    const destPath = path.join(__dirname, 'uploads', newName); // Corrected path concatenation
+    await imageDownloader.image({
+        url: link,
+        dest: destPath,
+    });
+    res.json(newName);
+});
+
+const photosMiddleware = multer({ dest: 'uploads/' });
+app.post('/upload', photosMiddleware.array('photos', 100), (req, res) => {
+    const uploadedFiles = [];
+    for (let i = 0; i < req.files.length; i++) {
+        const { path, originalname } = req.files[i];
+        const parts = originalname.split('.');
+        const ext = parts[parts.length - 1];
+        const newPath = path + '.' + ext;
+        fs.renameSync(path, newPath);
+        uploadedFiles.push(newPath.replace('uploads/', ''));
+    }
+    res.json(uploadedFiles);
+});
+
+app.post('/places', (req, res) => {
+    const { token } = req.cookies;
+    const {
+        title,
+        address,
+        addedPhotos,
+        description,
+        perks,
+        extraInfo,
+        checkIn,
+        checkOut,
+        maxGuests,
+    } = req.body;
+    jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+        if (err) throw err;
+        try {
+            const placeDoc = await Place.create({
+                owner: userData.id,
+                title,
+                address,
+                addedPhotos,
+                description,
+                perks,
+                extraInfo,
+                checkIn,
+                checkOut,
+                maxGuests,
+            });
+            res.json(placeDoc);
+        } catch (error) {
+            console.error('Error creating place:', error);
+            res.status(500).json({ error: 'Error creating place' });
+        }
+    });
+});
 
 app.listen(4000, () => {
     console.log('Server running on http://localhost:4000');
